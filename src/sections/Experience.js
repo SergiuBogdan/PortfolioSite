7541f5bad6fddb2e21a590c43d474d620ab7e8d1
Catana Sergiu Bogdan <sergiu.bogdan09@yahoo.com>
2023-12-25 20:54:17 +0200

    Add files via upload
    Repaired bug

--- src/sections/Experience.js
+++ src/sections/Experience.js
@@ -46,7 +46,6 @@ const Experience = () => {
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
-        viewport={{ once: true, amount: 0.5 }}
         className="flex items-start justify-evenly flex-wrap"
       >
         <div className="w-full md:w-80 lg:w-96 py-4 px-6">
